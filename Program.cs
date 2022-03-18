a3e05456abc36ff8302915991b645e23bcdd4637
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-17 21:17:24 -0500

    CSValuetest

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+
+
+
+Console.WriteLine( "Hey what is your name?" );
+string name = Console.ReadKey();
+Console.WriteLine($"Oh so your name is {name}? Well how old are you?");
+
+
